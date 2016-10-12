b3bb33efb61015975249ee945c481cd6673e660b
torevb <torevb@hotmail.com>
2016-10-12 12:54:49 +0200

    Ryddet opp i kommentarer.

--- exercise2/timer.c
+++ exercise2/timer.c
@@ -12,10 +12,6 @@ void setupTimer(uint16_t period)
 
 	/* Set amount of count ticks. */
 	*TIMER1_TOP = period;	//Sets timer period. 
-
-	/* Start/stop the timer. */
-	// This property has been distributed elsewhere in the code. Keep it here as a comment for future reference. 
-	//*TIMER1_CMD = 0x1;	//0x1 starts, 0x2 stops.
 }
 
 void setupTimerInterrupt()
@@ -24,16 +20,14 @@ void setupTimerInterrupt()
 	*TIMER1_IEN = 0x1;	//Enables overflow interrupt.
 }
 
+/* Enable Timer clock. */
 //void enableTimer()
 //{
-//	/* Enable Timer clock. */
 //	*CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_TIMER1;
 //}
 
+/* Disable Timer clock. */
 //void disableTimer()
 //{
-//	/* Disable Timer clock. */
 //	*CMU_HFPERCLKEN0 &= ~(CMU2_HFPERCLKEN0_TIMER1);
-//	/* Disable interrupt */
-//	//*TIMER1_IEN = 0x0;
 //}
