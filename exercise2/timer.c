c80a35cf8ae23d397208b08489096266cf577503
torevb <torevb@hotmail.com>
2016-10-12 15:52:09 +0200

    Fjernet start og stop timer sine funksjoner. Skriver heller bare rett til register.

--- exercise2/timer.c
+++ exercise2/timer.c
@@ -2,7 +2,6 @@
 #include <stdbool.h>
 
 #include "efm32gg.h"
-void startTimer();
 
 /* Enable internal timer in the program. Maximum input period is 0xFFFF. */
 void setupTimer(uint16_t period)
@@ -19,15 +18,3 @@ void setupTimerInterrupt()
 	*TIMER1_IFC = 0x1;	//Clears overflow interrupt.
 	*TIMER1_IEN = 0x1;	//Enables overflow interrupt.
 }
-
-/* Enable Timer clock. */
-//void enableTimer()
-//{
-//	*CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_TIMER1;
-//}
-
-/* Disable Timer clock. */
-//void disableTimer()
-//{
-//	*CMU_HFPERCLKEN0 &= ~(CMU2_HFPERCLKEN0_TIMER1);
-//}
