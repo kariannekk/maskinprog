f2d7f85c784bc652d1cb970edfa142e578f8778f
torevb <torevb@hotmail.com>
2016-10-16 14:51:33 +0200

    Kommentar.

--- exercise2/dac.c
+++ exercise2/dac.c
@@ -10,6 +10,6 @@ void setupDAC()
 
 	/* Enable input and output channels. */
 	*DAC0_CTRL = 0x50010;	//Prescale DAC clock with CMU clock divided by factor of 2⁵. DAC output to pin enabled. Add 0x4 for sample/hold instead of continuous conversion mode. 
-	*DAC0_CH0CTRL = 0x1;	//Enable right audio channels. 
-	*DAC0_CH1CTRL = 0x1;	//Enable left audio channels. 
+	*DAC0_CH0CTRL = 0x1;	//Enable right audio channel. 
+	*DAC0_CH1CTRL = 0x1;	//Enable left audio channel. 
 }
